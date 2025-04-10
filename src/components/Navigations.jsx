fa12a789d4b6efc0e6a68d2ad286f519cccad798
libbypieper <libbypieper@gmail.com>
2025-04-10 13:58:20 -0500

    draft.jsx

--- src/components/Navigations.jsx
+++ src/components/Navigations.jsx
@@ -1,1 +1,14 @@
 /* TODO - add your code to create a functional React component that renders a navigation bar for the different views in your single page application. You may consider conditionally rendering some options - for example 'Login' should be available if someone has not logged in yet. */
+
+ import { Link } from 'react-router-dom';
+
+ function Navigations() {
+     return <ul>
+       <Link to="/">Books</Link>
+       <Link to="/login">Login</Link>
+       <Link to ="/register">Register</Link>
+       <Link to ="/account">Account</Link>
+     </ul>;
+   }
+
+   export default Navigations;  
